b98e7034ff8539d932eb8e55d71ba7484e952993
vanshu0208 <101659689+vanshu0208@users.noreply.github.com>
2024-09-04 11:56:58 +0530

    Update project.py

--- project.py
+++ project.py
@@ -8,14 +8,26 @@ if len(x)==4:
     st.write('Login Successful')
     n+=1
     if n==2:
-    st.radio("Do you want to take Loan?",['Yes','No'])
-    a=st.number_input('Enter the loan amount: ')
-    t=st.number_input('Enter the time period: ')
-    st.write('INTEREST RATE: 6% p.a')
-    c=st.button('Calculate')
-    z=1
-    else:
-    st.write('Visit again')
+        st.radio("Do you want to take Loan?",['Yes','No'])
+        a=st.number_input('Enter the loan amount: ')
+        t=st.number_input('Enter the time period: ')
+        st.write('INTEREST RATE: 6% p.a')
+        c=st.button('Calculate')
+        z=1
+        if c==1:
+            SI=(a*6*t)/100
+            SI_final=a+SI
+            m=(a*6*t)/(100*12)
+            st.write('Total amount to be paid after given time is : ',SI_final)
+            st.write('Total yearly Interest is : ',SI)
+            st.write('Total monthly EMI is : ', m)
+            st.balloons()
+            z+=1
+            if z==2:
+                st.write('Pay EMI on time 😡')
+                st.write('Thanks for your visit')
+            else:
+                st.write('Visit again')
 
 elif len(x)==0:
     st.write('')
@@ -23,15 +35,4 @@ else:
     st.write('Re-enter')
 
 
-if c==1:
-    SI=(a*6*t)/100
-    SI_final=a+SI
-    m=(a*6*t)/(100*12)
-    st.write('Total amount to be paid after given time is : ',SI_final)
-    st.write('Total yearly Interest is : ',SI)
-    st.write('Total monthly EMI is : ', m)
-    st.balloons()
-    z+=1
-if z==2:
-    st.write('Pay EMI on time 😡')
-    st.write('Thanks for your visit')
+
